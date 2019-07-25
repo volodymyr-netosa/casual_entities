52bfb348c738b0157f6c5d182ecd1110d6e6f64c
Vladimir Netyosa <v.netyosa@Vladimirs-MacBook-Pro.local>
2019-07-25 21:04:02 +0300

    Added container logic, maybe it works

--- src/utility/Container.ts
+++ src/utility/Container.ts
@@ -1,15 +1,44 @@
+import { getEntitiesFromFolder } from './utility';
+
 export class Container {
     private entities = new Map();
     private entitiesInstances = new Map();
     private allowedDataTypes = ['string', 'number', 'boolean']
+
+    async init(entityFolderPath: string) {
+        return getEntitiesFromFolder(entityFolderPath).then(
+            (entities) => {
+                entities.forEach((name, props) => {
+                    if (this.verifyEntityProperties(props))
+                        this.entities.set(name, props)
+                    else console.log(`Adding ${name} entity failed. Not allowed data type`)   //TODO: add normal logger?
+                });
+            }
+        )
+    }
     
     addEntity(name: string, properties: any) {
         if (this.entities.has(name) || !this.verifyEntityProperties(properties)) return false;
         this.entities.set(name, properties);
     }
 
-    getEntityInstance(name: string) {
+    getEntityInstances(name: string) {
         return this.entitiesInstances.get(name);
+    }
+
+    addEntityInstance(entityName: string, instance: {}) {
+        if (!this.verityEntityInstance(entityName, instance)) {
+            console.log(`Cant add ${entityName} instance ${instance}, props type mismatching`); //TODO: logger ?Xd
+            return false;
+        }
+        if (this.entitiesInstances.has(entityName)) {
+            this.entitiesInstances.get(entityName).push(instance);
+        }
+        else this.entitiesInstances.set(entityName,[instance]);
+    }
+
+    getEntitiesNames(){
+        return Array.from(this.entitiesInstances.keys())
     }
 
     verifyEntityProperties(properties: any) {
@@ -20,8 +49,8 @@ export class Container {
         return true;
     }
 
-    verityEntityInstance(name: string, entityInstace: any) {
-        let entity = this.entities.get(name);
+    verityEntityInstance(entityName: string, entityInstace: any) {
+        let entity = this.entities.get(entityName);
         if (!entity) return false;
         for (let propName of Object.keys(entity)) {
             if (!(typeof entityInstace[propName] === entity[propName])) return false;
--- src/utility/Utility.ts
+++ src/utility/Utility.ts
@@ -1,11 +1,54 @@
 import * as fs from 'fs';
+import * as path from 'path';
 
 export async function parseJsonEntityFile(absolutePath: string) {
     return new Promise((resolve, reject) => {
         fs.readFile(absolutePath, (err, buffer) => {
             if (err) return reject(err);
             let entity = JSON.parse(buffer.toString());
-            resolve(entity);
+            let entityName = removeExtension(getFileNameFromPath(absolutePath));
+            resolve({
+                entity,
+                entityName
+            });
         });
     });
+}
+
+export async function getJsonFilesInFolder(folderPath: string) {
+    return new Promise((resolve, reject) => {
+        fs.readdir(folderPath, (err, files) => {
+            if (err) return reject(err);
+            let jsonFiles = files.filter((value) => value.match(/.json$/));
+            resolve(jsonFiles);
+        })
+    })
+}
+
+export async function getEntitiesFromFolder(folderPath: string) {
+    return getJsonFilesInFolder(folderPath).then((fileNames: any[]) => {
+        const entityPromices: any[] = []
+        fileNames.forEach(
+            (name: string) => {
+                entityPromices.push(
+                    parseJsonEntityFile(path.resolve(folderPath, name))
+                )
+            }
+        );
+        return Promise.all(entityPromices);
+    }).then((entitiesData) => {
+        let entities = new Map();
+        for (let data of entitiesData) {
+            entities.set(data.entityName, data.entity);
+        }
+        return entities;
+    })
+}
+
+function getFileNameFromPath(path: string): string {
+    return path.replace(/^.*[\\\/]/, '');
+};
+
+function removeExtension(fileName: string): string {
+    return fileName.split('.').slice(0, -1).join('.');
 }
